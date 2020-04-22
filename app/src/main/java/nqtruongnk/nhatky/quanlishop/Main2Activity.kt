daedc5840636409bea505b78684d9ff046030b8b
nqtruongnk <nqtruongnk@gmail.com>
2020-04-22 14:56:44 +0700

    làm login mà còn lỗi

--- app/src/main/java/nqtruongnk/nhatky/quanlishop/Main2Activity.kt
+++ app/src/main/java/nqtruongnk/nhatky/quanlishop/Main2Activity.kt
@@ -2,11 +2,26 @@ package nqtruongnk.nhatky.quanlishop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Main2Activity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser : FirebaseUser?){
+
     }
 }
--- app/src/main/java/nqtruongnk/nhatky/quanlishop/MainActivity.kt
+++ app/src/main/java/nqtruongnk/nhatky/quanlishop/MainActivity.kt
@@ -3,9 +3,17 @@ package nqtruongnk.nhatky.quanlishop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.UserHandle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +22,54 @@ class MainActivity : AppCompatActivity() {
         btnRegister.setOnClickListener {
             val intent: Intent= Intent(this, Register::class.java)
             startActivity(intent)
+            auth = FirebaseAuth.getInstance()
+        }
+
+        btnSingIn.setOnClickListener(){
+            signUpUser()
         }
     }
+
+    private fun signUpUser(){
+        if (edtUsername.text.toString().isEmpty()){
+            edtUsername.error = "Plesase Enter Email!"
+            edtUsername.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(edtUsername.text.toString()).matches()){
+            edtUsername.error = "Plesase Enter valid Email!"
+            edtUsername.requestFocus()
+            return
+        }
+
+        if (edtPassword.text.toString().isEmpty()){
+            edtPassword.error = "Plesase Enter Password!"
+            edtPassword.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(edtUsername.text.toString(), edtPassword.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this,Main2Activity::class.java))
+                    finish()
+                } else {
+                   Toast.makeText(baseContext, "Sign Up failed. Try again after some time.",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+                // ...
+            }
+
+    }
+
+
+
+
+
+
+
 
 
 }
